49acb0a28ba19301f3bb7cb624d6613a44c01209
Juliana Andrade Buono <66313307+buonoOwO@users.noreply.github.com>
2025-07-09 13:02:40 -0300

    Todas as funções necessárias

--- Jogo da Forca/funcoes_forca.h
+++ Jogo da Forca/funcoes_forca.h
@@ -1,7 +1,13 @@
-#define TAMANHO 25
+#define TAMANHO 35
 
 void abertura_jogo();
 void escolhe_tema();
-void escolhe_palavra();
-void chuta();
+void escolhe_palavra(const char* arquivo_desejado);
 bool verifica_chute(char chute);
+void chuta(char palavra[]);
+bool ja_chutou(char chute);
+void desenha_forca();
+void sublinha_palavra(char palavra[]);
+bool enforcou();
+bool ganhou(char palavra[]);
+void limpar_terminal();
