dbbcd5e1f0e5bdfd7baed5b6e1516fd939ab2971
Juliana Andrade Buono <66313307+buonoOwO@users.noreply.github.com>
2025-07-12 11:13:59 -0300

    Funções renovadas

--- Jogo da Forca/funcoes_forca.h
+++ Jogo da Forca/funcoes_forca.h
@@ -3,11 +3,11 @@
 void abertura_jogo();
 void escolhe_tema();
 void escolhe_palavra(const char* arquivo_desejado);
-bool verifica_chute(char chute);
-void chuta(char palavra[]);
+bool verifica_chute(char chute, int *tamanho);
+void chuta(char palavra[], int *tamanho);
 bool ja_chutou(char chute);
 void desenha_forca();
-void sublinha_palavra(char palavra[]);
+void sublinha_palavra(char palavra[], int *tamanho);
 bool enforcou();
 bool ganhou(char palavra[]);
 void limpar_terminal();
